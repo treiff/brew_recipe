e2e0e9bea1ee535eee43b372a562ca4feaa7061b
Trevor Reiff <trevorreiff@gmail.com>
2015-01-28 20:06:35 -0500

    Refactor page title, extract method to helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return full title on per-page basis.
+  def full_title(page_title)
+    base_title = "Brew Blog"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
