b00a57b486e3d5ddab6c9e2b5e109977ab4606bc
Trevor Reiff <trevorreiff@gmail.com>
2015-02-06 11:38:03 -0500

    Added flash messages sign in / sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,13 @@ class SessionsController < ApplicationController
     if params[:provider] == "facebook"
       user = User.from_omniauth(env["omniauth.auth"])
       log_in(user)
+      flash[:notice] = "Signed in successfully"
       redirect_to root_path
     else
       user = User.find_by_email(params[:session][:email])
       if user && user.authenticate(params[:session][:password])
         log_in(user)
+        flash[:notice] = "Signed in successfully"
         redirect_to root_path
       else
         render 'new'
@@ -21,6 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "Signed out successfully"
     redirect_to root_path
   end
 
