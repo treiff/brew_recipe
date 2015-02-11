5c74e81ad306626fc5dd1d49def6656893b0f8a3
Trevor Reiff <trevorreiff@gmail.com>
2015-02-11 11:21:42 -0500

    Implement users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    context 'with invalid credentials' do
+      it 'renders new action' do
+        user = create(:user)
+        post :create, user: { email: user.email, password: user.password, password_confirmation: 'ut-oh' }
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context 'with valid credentials' do
+      it 'creates user' do
+        user = create(:user)
+
+        expect{ post :create, user: attributes_for(:user) }.to change(User, :count).by(1)
+      end
+
+      it 'redirects to root path' do
+        user = create(:user)
+        post :create, user: attributes_for(:user)
+
+        expect(flash[:notice]).to match(/^Welcome to brewblog/)
+        expect(response).to redirect_to root_path
+      end
     end
   end
 
