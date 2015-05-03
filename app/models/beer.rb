2e1b58efe2d6c021b31fc2753fedc40723d713af
Trevor Reiff <trevorreiff@gmail.com>
2015-05-03 13:46:36 -0400

    Work on xml parsing

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,5 +3,21 @@ class Beer < ActiveRecord::Base
   has_attached_file :beer_xml, bucket: 'brewrecipes'
   validates_attachment_content_type :beer_xml, content_type: 'text/xml'
 
+  before_save :parse_file
+
   belongs_to :user
+
+  private
+
+    def parse_file
+      temp = beer_xml.queued_for_write[:original]
+      doc = Nokogiri::XML(temp)
+      extract_data(doc)
+    end
+
+    def extract_data(doc)
+      doc.root.elements.each do |element|
+        p "element is: " + element
+      end
+    end
 end
