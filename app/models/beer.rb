4d67726ba75aad8a06179c48421e6dd3f72c2522
Trevor Reiff <trevorreiff@gmail.com>
2015-05-07 20:13:20 -0400

    Modify before save callback

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -9,15 +9,24 @@ class Beer < ActiveRecord::Base
 
   private
 
-    def parse_file
-      temp = beer_xml.queued_for_write[:original]
-      doc = Nokogiri::XML(temp)
-      extract_data(doc)
-    end
+  def parse_file
+    temp = beer_xml.queued_for_write[:original]
+    doc = Nokogiri::XML(temp)
+    extract_data(doc)
+  end
 
-    def extract_data(doc)
-      doc.root.elements.each do |element|
-        p "element is: " + element
-      end
+  def extract_data(doc)
+    doc.xpath("//RECIPE").each do |node|
+      self.name = node.at_xpath("NAME").value
+      self.type = node.at_xpath("TYPE")
+      self.est_eff = node.at_xpath("EFFICIENCY")
+      self.style = node.at_xpath("STYLE//NAME")
+      self.style_category = node.at_xpath("STYLE//CATEGORY")
+      self.og = node.at_xpath("EST_OG")
+      self.fg = node.at_xpath("EST_FG")
+      self.srm = node.at_xpath("EST_COLOR")
+      self.ibu = node.at_xpath("IBU")
+      self.abv = node.at_xpath("EST_ABV")
     end
+  end
 end
