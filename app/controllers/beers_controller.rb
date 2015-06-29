5762659ea0ad6822fab0549fac30728ee59e361c
Trevor Reiff <trevorreiff@gmail.com>
2015-06-29 14:49:29 -0400

    Fixed rubocop error

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,5 @@
 class BeersController < ApplicationController
-#  before_action :logged_in_user, only: [:index]
+  #  before_action :logged_in_user, only: [:index]
 
   def index
     if params[:q].present?
