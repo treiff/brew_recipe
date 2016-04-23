bfd32b86a4db15f0934435111df3ba795b9fb186
Trevor Reiff <trevorreiff@gmail.com>
2016-04-23 14:25:39 -0400

    Disable CORS for testing locally

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ Bundler.require(*Rails.groups)
 
 module Brewrecipe
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
