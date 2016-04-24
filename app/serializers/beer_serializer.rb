5989524daa8e607b09d1516065ea90667185ee57
Trevor Reiff <trevorreiff@gmail.com>
2016-04-23 23:02:52 -0400

    Fix lint errors

--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,3 +1,4 @@
+# Serializer for Beer model
 class BeerSerializer < ActiveModel::Serializer
   attributes :id, :name, :style
 end
--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Brewrecipe
   class Application < Rails::Application
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      'Access-Control-Request-Method' => %w(GET POST OPTIONS).join(",")
     }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
