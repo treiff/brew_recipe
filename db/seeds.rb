f7dc89bc7fcf2e5f986d47ed3e6534d0f7f856c3
Trevor Reiff <trevorreiff@gmail.com>
2016-04-30 20:00:49 -0400

    Added quick seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Beer.create([
+  {name: 'Row 2 Hill 56', style: 'APA', og: 1.056, fg: 1.010},
+  {name: 'Boston Lager', style: 'Vienna Lager', og: 1.048, fg: 1.009},
+  {name: 'Sam Smith Oatmeal Stout', style: 'Stout', og: 1.058, fg: 1.012},
+  {name: 'Harpoon IPA', style: 'American IPA', og: 1.060, fg: 1.012},
+  {name: 'Heady Topper', style: 'IIPA', og: 1.080, fg: 1.012},
+  {name: 'Big Eye IPA', style: 'American IPA', og: 1.065, fg: 1.014},
+  {name: 'Pale Ale', style: 'APA', og: 1.056, fg: 1.010},
+  {name: 'Unearthed', style: 'American Stout', og: 1.066, fg: 1.010},
+  {name: '2xIPA', style: 'IPA', og: 1.081, fg: 1.010}
+])
